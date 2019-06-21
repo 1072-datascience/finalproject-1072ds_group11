83ff1298bbc71cebf0b442dcfe236e42532f9df8
dkmsfire <leozxc2009@gmail.com>
2019-06-22 01:45:11 +0800

    at ANN model, that is too waste time, need to optimize

--- code/JPY.r
+++ code/JPY.r
@@ -1,9 +1,11 @@
+### packages
 library(randomForest)
 library(neuralnet)
 library(nnet)
 library(caret)
 library(kernlab)
-#load data and add and adjust new variables
+
+### load data and add and adjust new variables
 d <- read.csv('data/JPY1.csv', header = T)
 d$year <- factor(substr(d$Date, 1, 4))
 d$dir <- factor(c(NA,ifelse(tail(d$Close,-1)-head(d$Close,-1)>=0,1,0)))
@@ -14,8 +16,9 @@ for (i in vars){
   d[name] <- (d[,i]-min(d[,i],na.rm = TRUE))/(max(d[,i],na.rm = TRUE)-min(d[,i],na.rm = TRUE))
   norm_vars <- c(norm_vars,name)
 }
-#parameter selection data
-set.seed(999)
+
+### stratified random sampling 
+set.seed(777)
 for (y in setdiff(levels(d$year),'2000')){
   index_sample1 <- sample(rownames(subset(d,year==y & dir==1)),round(0.2*length(rownames(subset(d,year==y & dir==1)))))
   index_sample0 <- sample(rownames(subset(d,year==y & dir==0)),round(0.2*length(rownames(subset(d,year==y & dir==0)))))
@@ -36,7 +39,10 @@ for (y in setdiff(levels(d$year),'2000')){
     d_ps_holdout <- rbind(d_ps_holdout,d_parameter_selection[setdiff(rownames(subset(d_parameter_selection,year==y)),c(sort(c(index_sample1,index_sample0),decreasing = FALSE))),c('dir',norm_vars)])
   }
 }
-#choose ANN parameters
+
+
+### train model with stratified random sampling data
+# train ANN model and record tuning parameters
 ann_ps_pred_train <- data.frame(n=factor(),accuracy_train=factor())
 ann_ps_pred_holdout <- data.frame(n=factor(),accuracy_holdout=factor())
 for (n in seq(10,100,10)){
@@ -51,7 +57,8 @@ for (n in seq(10,100,10)){
   ann_ps_pred$mean <- (ann_ps_pred$accuracy_train+ann_ps_pred$accuracy_holdout)/2
 }
 ann_parameter <- ann_ps_pred[order(ann_ps_pred$mean,decreasing = TRUE),][1:3,1]
-#choose SVM parameters
+
+# train SVM model and record tuning parameters
 svm_ps_pred_train <- data.frame(kernel=factor(),degree=factor(),gamma=factor(),c=factor(),accuracy_train=factor())
 svm_ps_pred_holdout <- data.frame(kernel=factor(),degree=factor(),gamma=factor(),c=factor(),accuracy_holdout=factor())
 for (kernel in c('polydot','rbfdot')){
@@ -84,7 +91,8 @@ for (kernel in c('polydot','rbfdot')){
   svm_ps_pred$mean <- (svm_ps_pred$accuracy_train+svm_ps_pred$accuracy_holdout)/2
 }
 svm_parameter <- rbind(subset(svm_ps_pred,kernel=='polydot')[order(subset(svm_ps_pred,kernel=='polydot')$mean,decreasing = TRUE),][1,1:4],subset(svm_ps_pred,kernel=='rbfdot')[order(subset(svm_ps_pred,kernel=='rbfdot')$mean,decreasing = TRUE),][1,1:4])
-#choose randomForest parameters
+
+# train random forest model and record tuning parameters
 rf_ps_pred_train <- data.frame(ntree=factor(),accuracy_train=factor())
 rf_ps_pred_holdout <- data.frame(ntree=factor(),accuracy_holdout=factor())
 for (i in seq(10,200,10)){
@@ -99,7 +107,10 @@ for (i in seq(10,200,10)){
   rf_ps_pred$mean <- (rf_ps_pred$accuracy_train+rf_ps_pred$accuracy_holdout)/2
 }
 rf_parameter <- rf_ps_pred[order(rf_ps_pred$mean,decreasing = TRUE),][1:3,1]
-#Comparison
+
+# train Naive Bayes model and record tuning parameters
+
+# Comparison of these models
 for (y in setdiff(levels(d$year),'2000')){
   index_sample1 <- sample(rownames(subset(d,year==y & dir==1)),round(0.5*length(which(d$year==y & d$dir==1))))
   index_sample0 <- sample(rownames(subset(d,year==y & dir==0)),round(0.5*length(which(d$year==y & d$dir==0))))
@@ -111,6 +122,8 @@ for (y in setdiff(levels(d$year),'2000')){
     d_holdout <- rbind(d_holdout,d[setdiff(rownames(subset(d,year==y)),c(sort(c(index_sample1,index_sample0),decreasing = FALSE),rownames(subset(d,year=='2000')))),c('dir',norm_vars)])
   }
 }
+
+### using the stratified model's tuning parameters to build the same tuning parameters model for the bigger train datasets.
 #ANN
 ann_performance <- data.frame(n=factor(),accuracy_train=factor(),accuracy_holdout=factor())
 for (n in ann_parameter){
@@ -124,6 +137,7 @@ for (n in ann_parameter){
   ann_performance <- rbind(ann_performance,data.frame(n=n,accuracy_train=accuracy_train,accuracy_holdout=accuracy_holdout))
 }
 ann_performance$mean <- (as.numeric(ann_performance$accuracy_train)+as.numeric(ann_performance$accuracy_holdout))/2
+
 #SVM
 svm_performance <- data.frame(kernel=factor(),degree=factor(),gamma=factor(),c=factor(),accuracy_train=factor(),accuracy_holdout=factor())
 svm_model <- ksvm(dir~., data=d_train, kernel=as.character(svm_parameter[1,1]), degree=as.integer(svm_parameter[1,2]), C=as.integer(svm_parameter[1,4]))
@@ -143,6 +157,7 @@ accuracy_train <- sum(diag(table(svm_result_train))/dim(d_train)[1])
 accuracy_holdout <- sum(diag(table(svm_result_holdout))/dim(d_holdout)[1])
 svm_performance <- rbind(svm_performance,data.frame(kernel=svm_parameter[2,1],degree=NA,gamma=svm_parameter[2,3],c=svm_parameter[2,4],accuracy_train=accuracy_train,accuracy_holdout=accuracy_holdout))
 svm_performance$mean <- (as.numeric(svm_performance$accuracy_train)+as.numeric(svm_performance$accuracy_holdout))/2
+
 #randomForest
 rf_performance <- data.frame(ntree=factor(),accuracy_train=factor(),accuracy_holdout=factor())
 for (n in rf_parameter){
@@ -156,5 +171,6 @@ for (n in rf_parameter){
   rf_performance <- rbind(rf_performance,data.frame(ntree=n,accuracy_train=accuracy_train,accuracy_holdout=accuracy_holdout))
 }
 rf_performance$mean <- (as.numeric(rf_performance$accuracy_train)+as.numeric(rf_performance$accuracy_holdout))/2
+
 #table
 compare_list <- list(ANN=ann_performance,SVM=svm_performance,RandomForest=rf_performance)
