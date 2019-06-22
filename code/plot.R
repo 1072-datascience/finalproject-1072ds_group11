d781ef38f2aa509f91116229bc687d9ef5309f89
dkmsfire <leozxc2009@gmail.com>
2019-06-23 01:16:33 +0800

    try to draw plots about continuous data and discrete data with fill = direction

--- code/plot.R
+++ code/plot.R
@@ -1,6 +1,21 @@
+### packages
+library(caret)
+library(gridExtra)
 ### in this R code, we will produce the plots that need to show in presentation.
 
 ### index increasing or decreasing per year
+load("Japan.rda")
 
+for(i in c(7:16, 21:30)){
+  png(filename = paste0("png/", names(d)[[i]], ".png"))
+  print(ggplot(data = d, aes(x = c(1:nrow(d)), y = d[[i]], color = d$dir)) +
+    geom_line() + ylab(names(d)[[i]]) + xlab("Date"))
+  dev.off()
+}
+for(i in 31:39){
+  png(filename = paste0("png/", names(d)[[i]], ".png"))
+  print(ggplot(data = d, aes(d[[i]], ..count..)) + geom_bar(aes(fill = d$dir)) + xlab(names(d)[[i]]))
+  dev.off()
+}
 
 ###
